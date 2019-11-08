c90f8e034058d808934a1eed91c8ba59759170c7
RonnyLV <hamste126@gmail.com>
2019-11-09 00:05:14 +0200

    Dependencies from requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from pip.req import parse_requirements
-from setuptools import setup, find_packages
 from os import path
+
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
 setup(
     name='prnet',
     version='0.1',
     packages=find_packages(include=['prnet']),
     python_requires='>=3.6, <4',
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=install_requires,
     project_urls={  # Optional
         'Source': 'https://github.com/RonnyLV/PRNet/',
     },
